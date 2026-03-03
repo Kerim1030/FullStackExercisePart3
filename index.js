469181f36422cac478f2d49bb2144ef4cf1f100d
Kerim1030 <kaplanerkerim891@gmail.com>
2026-03-03 11:59:18 +0100

    Exercice 3.7 et 3.8

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express()
 
 app.use(express.json())
 
+const morgan = require('morgan')
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 let persons = [
     { 
       "id": 1,
